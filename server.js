77934f9515c8452078366a94be58a63d82b7f45c
Victor Chung <victochung998@yahoo.com>
2019-11-02 18:49:25 -0700

    correct the scrape link

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  /* axios.get("http://www.echojs.com/").then(function(response) { */
+   axios.get("http://www.echojs.com/").then(function(response) { 
 
-    axios.get("https://www.npr.org/").then(function(response) { 
+    /* axios.get("https://www.npr.org/").then(function(response) {  */
 
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
@@ -53,15 +53,15 @@ app.get("/scrape", function(req, res) {
         .attr("href");
       console.log(result)
       // Create a new Article using the `result` object built from scraping
-      // db.Article.create(result)
-      //   .then(function(dbArticle) {
-      //     // View the added result in the console
-      //     console.log(dbArticle);
-      //   })
-      //   .catch(function(err) {
-      //     // If an error occurred, log it
-      //     console.log(err);
-      //   });
+       db.Article.create(result)
+         .then(function(dbArticle) {
+          // View the added result in the console
+           console.log(dbArticle);
+         })
+         .catch(function(err) {
+           // If an error occurred, log it
+           console.log(err);
+         }); 
     });
 
     // Send a message to the client
